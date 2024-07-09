5915ec7cc93a7784ebc2122ab3763d48d825433c
Keoma Kevin Baudin <keoma4@gmail.com>
2024-07-09 00:45:26 -0300

    Aula 1

---
+++ src/Journey.Exception/ExceptionsBase/JourneyException.cs
@@ -0,0 +1,19 @@
+﻿namespace Journey.Exception.ExceptionsBase;
+
+public class JourneyException : System.Exception
+{
+
+    public JourneyException()
+    {
+    }
+
+    public JourneyException(string message)
+        : base(message)
+    {
+    }
+
+    public JourneyException(string message, System.Exception inner)
+        : base(message, inner)
+    {
+    }
+}
